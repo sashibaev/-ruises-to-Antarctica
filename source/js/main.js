dbe6f64bf95d3a806ccc3c7baf316dbbb5a980ac
Sergey Shibaev <srgjjshibaev@rambler.ru>
2020-10-29 19:03:28 +0300

    Дорабатывает js, вывод логотипа в IE11

--- source/js/main.js
+++ source/js/main.js
@@ -4,7 +4,8 @@ window.main = (function () {
   var menu = document.querySelector('.page-header');
   var button = menu.querySelector('.page-header__toggle-close');
   var mainNav = menu.querySelector('.page-header__nav');
-  var fillSvg = document.querySelector('.page-header__logo-svg');
+  var logoCloseMenu = document.querySelector('.page-header__logo-svg');
+  var logoOpenMenu = document.querySelector('.page-header__logo-menu');
 
   if (button) {
     button.classList.add('page-header__toggle-open');
@@ -18,14 +19,19 @@ window.main = (function () {
     menu.classList.add('page-header__close');
   }
 
-  if (fillSvg) {
-    fillSvg.classList.remove('page-header__logo-close');
+  if (logoCloseMenu) {
+    logoCloseMenu.classList.add('page-header__logo-svg-open');
+  }
+
+  if (logoOpenMenu) {
+    logoOpenMenu.classList.add('page-header__logo-menu-close');
   }
 
   button.addEventListener('click', function () {
     button.classList.toggle('page-header__toggle-open');
     mainNav.classList.toggle('page-header__nav--hidden');
     menu.classList.toggle('page-header__close');
-    fillSvg.classList.toggle('page-header__logo-close');
+    logoCloseMenu.classList.toggle('page-header__logo-svg-open');
+    logoOpenMenu.classList.toggle('page-header__logo-menu-close');
   });
 })();
